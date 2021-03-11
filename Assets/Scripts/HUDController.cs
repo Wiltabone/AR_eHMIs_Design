e6867227c17ff1a151faf1cfbfa46bb5e335f3f3
Wiltabone <wiltabone@gmail.com>
2021-03-11 13:39:41 +0100

    Concept_2_8_9
    Added Concepts 2, 8 and 9. Still not finalised.

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+/*This class controls the HUD used in Concept 8. It changes the colour from
+ * red to green, text from unsafe to safe and the icon. 
+ - Created: 10/3/2021*/
+
+public class HUDController : MonoBehaviour
+{
+
+    public Color newColor;
+    public Sprite newSprite;
+    public string newText;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0)
+        {
+            print("Lubbu");
+            Image img = GameObject.Find("Panel").GetComponent<Image>();
+            img.color = newColor;
+
+            Image currSprite = GameObject.Find("Image").GetComponent<Image>();
+            currSprite.sprite = newSprite;
+
+            Text currText = GameObject.Find("Text").GetComponent<Text>();
+            currText.fontSize = 14;
+            currText.horizontalOverflow = HorizontalWrapMode.Wrap;
+            currText.verticalOverflow = VerticalWrapMode.Overflow;
+            currText.text = newText;
+            currText.text = "SAFE TO\nCROSS\n";
+            //currText.text = "CAREFUL\nA VEHICLE IS\nAPPROACHING!";
+        }
+    }
+}
---
+++ Assets/Scripts/SetMat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMat : MonoBehaviour
+{
+    public Material new_mat;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0)
+        {
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material = new_mat;
+        }
+    }
+}
