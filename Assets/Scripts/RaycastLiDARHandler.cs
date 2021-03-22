645e1dea1690655d5ec2e9afd24fef9249cd50c8
Wiltabone <wiltabone@gmail.com>
2021-03-22 13:26:07 +0100

    Commit EOW March 3
    Added Skeleton for Concepts 4 and 7. Distance working for Concept 3.

---
+++ Assets/Scripts/RaycastLiDARHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class RaycastLiDARHandler : MonoBehaviour
+{
+    public ARRaycastManager arRaycastManager;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private Vector2 rayPos = new Vector2(Screen.width/2, Screen.height/2);
+
+    private float distance;
+
+    
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+
+        //if (Input.touchCount == 0)
+        //return;
+
+        if (arRaycastManager.Raycast(rayPos, hits, TrackableType.Planes))
+        //if (arRaycastManager.Raycast(rayPos, hits))
+        {
+            distance = hits[0].distance;
+            print("DISTANCE: "+distance);
+        }
+    }
+}
